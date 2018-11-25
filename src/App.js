9cc466b51daebd9ba3dd030eaa4723a0c3d17f1b
Thomas Flack <thomas.flack@skybettingandgaming.com>
2018-11-25 22:20:25 +0000

    my commit here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const fetchApi = () =>
+  fetch('http://localhost:9002/')
+    .then(res => res.ok ? res.json() : Promise.reject('error'))
 
 class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount = () => {
+    fetchApi()
+      .then(data => this.setState({ data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { data } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {data ? JSON.stringify(data) : 'No data here brp'}
       </div>
     );
   }
